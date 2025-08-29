bebc0af8263d30912b81f5bf9e3560579f8b815c
jaclark5 <jennifer.clark@openforcefield.org>
2025-08-29 09:46:54 -0400

    Update typing and write to log file

--- 2_filtered_results/run_filter_data.sh
+++ 2_filtered_results/run_filter_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download and Process SPICE2 Dataset from Zenodo
-python filter_data.py --data-dir "../1_data/raw-spice"
+python filter_data.py --data-dir "../1_data/raw-spice" 2>&1 | tee log.txt
 
 # From output Finlay's cutoff of everything in the 95th percentile
 # corresponds to a Z-score of 1, with RMS Force of ~32 kcal/mol
