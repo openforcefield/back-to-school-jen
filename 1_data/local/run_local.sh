dbf2cd5bf32060465c75438e5c1556ae09b32dbb
jaclark5 <jennifer.clark@openforcefield.org>
2025-09-11 16:51:12 -0400

    Rerun to validate steps 1 and 2

--- 1_data/local/run_local.sh
+++ 1_data/local/run_local.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ######## (MVP) Download and Process SPICE2 Dataset from Zenodo #######
-python ../get_data_spice2.py --data-dir "." 2>&1 | tee log.txt
+python ../get_data_spice2.py --data-dir "../" --output-dir "." 2>&1 | tee log.txt
