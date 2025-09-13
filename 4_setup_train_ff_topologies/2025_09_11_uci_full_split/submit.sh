ba4e2b48d9c61e14499a2fa3705f68376117ff16
Jennifer Clark <jennifer.clark@omsf.io>
2025-09-13 05:11:19 -0700

    Run full dataset on uci

--- 4_setup_train_ff_topologies/2025_09_11_uci_full_split/submit.sh
+++ 4_setup_train_ff_topologies/2025_09_11_uci_full_split/submit.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
 #SBATCH --job-name=MakeInterchanges  ## job name
-#SBATCH -p free              ## use free partition
+#SBATCH -p standard              ## use free partition
 #SBATCH -t 1-00:00:00
 #SBATCH --nodes=1            ## use 1 node, don't ask for multiple
 #SBATCH --ntasks 1
-#SBATCH --cpus-per-task=32
-#SBATCH --mem-per-cpu=4G     ## ask for 1Gb memory per CPU
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=5G
 #SBATCH --constraint=fastscratch
-#SBATCH -o stdout.txt
-#SBATCH -e stderr.txt
+#SBATCH -o stdout_multi.txt
+#SBATCH -e stderr_multi.txt
 
 date
 hn=`hostname`
 echo "Running job on host $hn"
-ncpus = $SLURM_CPUS_ON_NODE
+ncpus=$SLURM_CPUS_ON_NODE
+echo "$ncpus allocated CPUs"
 
 source ~/.bashrc
 micromamba activate descent-workflow
 
 # Setup and save topologies for fitting
 python -u ../setup_train_ff_topo.py --data-dir "../../3_split_train_test/full_split_uci/data-train" \
-                                    --offxml "/dfs9/dmobley-lab/jclark9/descent/openff-2.2.1.offxml" \
+                                    --offxml "../../forcefields/openff_unconstrained-2.3.0-rc1.offxml" \
                                     --file-format "pkl" \
-                                    --device "cuda" \
-			                        --n-cpus $SLURM_CPUS_ON_NODE \
-                                    --precision "single" 2>&1 | tee log.txt
+                                    --device "cpu" \
+			            --n-cpus $SLURM_CPUS_ON_NODE \
+                                    --precision "single" 2>&1 | tee log_multi.txt
