6b57c615806a348b0783203ae4b61c2800f62b66
Jennifer Clark <jennifer.clark@omsf.io>
2025-10-30 14:09:16 -0700

    Update offxml production attempts

--- 5_make_offxmls/a_broad_specification/submit.sh
+++ 5_make_offxmls/a_broad_specification/submit.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 #SBATCH --job-name=a_ff  ## job name
-#SBATCH -p free              ## use free partition
+#SBATCH -p standard              ## use free partition
 #SBATCH -t 3-00:00:00
 #SBATCH --nodes=1            ## use 1 node, don't ask for multiple
 #SBATCH --ntasks 1
-#SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=20G
 #SBATCH --constraint=fastscratch
-#SBATCH --account DMOBLEY_LAB_GPU
+#SBATCH --account DMOBLEY_LAB
 #SBATCH -o stdout_multi.txt
 #SBATCH -e stderr_multi.txt
 
@@ -24,6 +23,7 @@ micromamba activate bts
 # Fit filtered training data
 python ../make_offxml.py --data-dir "../../../back-to-school-jen_step4/3_split_train_test/full_split_uci/data-train" \
                          --specificity-json "specificity.json" \
+			 -n $ncpus \
                          --filename-offxml-in "../../forcefields/openff_unconstrained-2.2.1.offxml" \
                          --filename-offxml-out "openff-2.2.1-ring-no-bond.offxml" \
                          --filename-test-train-smiles "../../../back-to-school-jen_step4/3_split_train_test/full_split_uci/smiles_test_train.json" \
