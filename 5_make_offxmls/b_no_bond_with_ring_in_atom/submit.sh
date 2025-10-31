0c7c10b0204aace0b41a9b89f1d4542f5a43c3df
Jennifer Clark <jennifer.clark@omsf.io>
2025-10-31 13:01:32 -0700

    Run b_* on uci

--- 5_make_offxmls/b_no_bond_with_ring_in_atom/submit.sh
+++ 5_make_offxmls/b_no_bond_with_ring_in_atom/submit.sh
@@ -21,12 +21,14 @@ source ~/.bashrc
 micromamba activate bts
 
 # Fit filtered training data
-python ../make_offxml.py --data-dir "../../../back-to-school-jen_step4/3_split_train_test/full_split_uci/data-train" \
+python ../make_offxml.py --data-dir "../../3_split_train_test/full_split_uci/data-train" \
                          --specificity-json "specificity.json" \
 			 -n $ncpus \
                          --filename-offxml-in "../../forcefields/openff_unconstrained-2.3.0-rc2.offxml" \
                          --filename-offxml-out "openff-2.2.1-ring-no-bond.offxml" \
-                         --filename-test-train-smiles "../../../back-to-school-jen_step4/3_split_train_test/full_split_uci/smiles_test_train.json" \
+                         --filename-test-train-smiles "../../3_split_train_test/full_split_uci/smiles_test_train.json" \
                          -vvv \
                          --datasets "OpenFF Industry Benchmark Season 1 v1.2" \
                          --datasets-type optimization 2>&1 | tee log.txt
+echo "\nRun complete"
+date
