eccd5f6c37404228b5dd755a28a8524d1867a3e5
jaclark5 <jennifer.clark@openforcefield.org>
2025-12-08 11:41:06 -0500

    Refactor repo for pkl files for each ff

---
+++ 5_setup_train_ff_topologies/2025_12_08_uci_off_a/submit.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH --job-name=MakeInterchanges  ## job name
+#SBATCH -p standard              ## use free partition
+#SBATCH -t 1-00:00:00
+#SBATCH --nodes=1            ## use 1 node, don't ask for multiple
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task=32
+#SBATCH --account DMOBLEY_LAB
+#SBATCH --mem-per-cpu=4G     ## ask for 1Gb memory per CPU
+#SBATCH --constraint=fastscratch
+#SBATCH -o stdout.txt
+#SBATCH -e stderr.txt
+
+date
+hn=`hostname`
+echo "Running job on host $hn"
+
+source ~/.bashrc
+micromamba activate descent-workflow
+
+# Setup and save topologies for fitting
+python -u ../setup_train_ff_topo.py --data-dir "../../3_split_train_test/full_split_uci/data-train" \
+                                    --offxml "/dfs9/dmobley-lab/jclark9/descent/back-to-school-jen/4_make_offxmls/a_broad_specification/openff-2.2.1-ring-no-bond.offxml" \
+                                    --file-format "pkl" \
+                                    --device "cpu" \
+			            --n-cpus $SLURM_CPUS_ON_NODE \
+                                    --precision "single" 2>&1 | tee log.txt
