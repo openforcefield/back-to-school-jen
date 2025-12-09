f2c39c74a9c0ba222f831986cd43163f90f7034a
Jennifer Clark <jennifer.clark@openforcefield.org>
2025-12-09 07:58:56 -0800

    Update fitting

--- 6_run_fit/0_openff_2.2.1/submit.sh
+++ 6_run_fit/0_openff_2.2.1/submit.sh
@@ -32,9 +32,9 @@ echo "$(which python)"
 python -c "import torch; print(f'CUDA available: {torch.cuda.is_available()}'); print(f'CUDA version: {torch.version.cuda}'); print(f'Number of GPUs: {torch.cuda.device_count()}')"
 
 python ../fit_data.py --data-dir "../../3_split_train_test/full_split_uci/data-train" \
-                      --filename-forcefield "../../5_setup_train_ff_topologies/2025_12_08_uci_off_a/smee_force_field.pkl" \
-                      --filename-topo-dict "../../5_setup_train_ff_topologies/2025_12_08_uci_off_a/smee_topology_dict.pkl" \
-                      --offxml "../../4_make_offxmls/a_broad_specification/openff-2.2.1-ring-no-bond.offxml" \
+                      --filename-forcefield "../../4_setup_train_ff_topologies/2025_09_11_uci_full_split/smee_force_field.pkl" \
+                      --filename-topo-dict "../../4_setup_train_ff_topologies/2025_09_11_uci_full_split/smee_topology_dict.pkl" \
+                      --offxml "../../5_make_offxmls/a_broad_specification/openff-2.2.1-ring-no-bond.offxml" \
                       --n-epochs 1000 \
 		      --to-cuda true \
                       --learning-rate 0.001 2>&1 | tee log.txt
--- 6_run_fit/b_no_bond_with_ring_in_atom/submit.sh
+++ 6_run_fit/b_no_bond_with_ring_in_atom/submit.sh
@@ -32,9 +32,9 @@ echo "$(which python)"
 python -c "import torch; print(f'CUDA available: {torch.cuda.is_available()}'); print(f'CUDA version: {torch.version.cuda}'); print(f'Number of GPUs: {torch.cuda.device_count()}')"
 
 python ../fit_data.py --data-dir "../../3_split_train_test/full_split_uci/data-train" \
-                      --filename-forcefield "../../5_setup_train_ff_topologies/2025_12_08_uci_off_a/smee_force_field.pkl" \
-                      --filename-topo-dict "../../5_setup_train_ff_topologies/2025_12_08_uci_off_a/smee_topology_dict.pkl" \
-                      --offxml "../../4_make_offxmls/a_broad_specification/openff-2.2.1-ring-no-bond.offxml" \
+                      --filename-forcefield "../../5_setup_train_ff_topologies/2025_12_08_uci_off_b/smee_force_field.pkl" \
+                      --filename-topo-dict "../../5_setup_train_ff_topologies/2025_12_08_uci_off_b/smee_topology_dict.pkl" \
+                      --offxml "../../4_make_offxmls/b_no_bond_with_ring_in_atom/openff-2.2.1-ring-no-bond.offxml" \
                       --n-epochs 1000 \
 		      --to-cuda true \
                       --learning-rate 0.001 2>&1 | tee log.txt
