994cb4035b00fcc655635ca5dff6a8b0e49c520d
Jennifer Clark <jennifer.clark@openforcefield.org>
2026-01-05 08:38:59 -0800

    Update force field comparison

--- 6_run_fit/compare_b_b2/run_compare_checkpoints.sh
+++ 6_run_fit/compare_b_b2/run_compare_checkpoints.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 python ../compare_checkpoints.py \
-    --checkpoint-first ../b_no_bond_with_ring_in_atom/final-force-field.offxml \
-    --checkpoint-last ../b2_no_bond_with_ring_in_atom/final-force-field.offxml \
+    --checkpoint-first ../b_no_bond_with_ring_in_atom/my-smee-fit/final-force-field.pt \
+    --checkpoint-last ../b2_no_bond_with_ring_in_atom/my-smee-fit/final-force-field.pt \
     --output-dir checkpoint_analysis/ 2>&1 | tee compare_checkpoints_log.txt
