341278f5ecf695bf418cff1ea380fffee8ca4e04
Jennifer Clark <jennifer.clark@openforcefield.org>
2026-01-07 07:07:08 -0800

    Rerun interchanges with SPICE for openff-2.3-rc2

--- 5_setup_train_ff_topologies/2025_09_11_uci_full_split/run_setup_topo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python -u ../setup_train_ff_topo.py --data-dir "../../3_split_train_test/full_split/data-train" \
-                                    --offxml "/Users/jenniferclark/bin/sage-2.2.1/openff-2.2.1.offxml" \
-                                    --file-format "pkl" \
-                                    --device "cuda" \
-			                        --n-cpus 1 \
-                                    --precision "single" 2>&1 | tee log.txt
