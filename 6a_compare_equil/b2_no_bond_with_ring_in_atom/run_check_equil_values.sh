7eb57527a48d718ae3db90702fd0a57620e8b765
Jennifer A Clark <jennifer.clark@omsf.io>
2026-01-22 17:46:04 -0500

    Compare equilibrium bond and angles values with data (#51)
    * Add files to create Huggingface dataset from Sage data
    
    * Update equ calculation to include sage training
    
    ---------
    
    Co-authored-by: jaclark5 <jennifer.clark@openforcefield.org>

---
+++ 6a_compare_equil/b2_no_bond_with_ring_in_atom/run_check_equil_values.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export OMP_NUM_THREADS=1
+N_PROCESSES=${SLURM_CPUS_PER_TASK:-1}
+
+echo "Running with ${N_PROCESSES} processes"
+
+# Run analysis
+python -u ../check_equil_values.py \
+    --offxml "../../6_run_fit/b2_no_bond_with_ring_in_atom/final-force-field.offxml" \
+    --dataset-paths "../../3_split_train_test/full_split_uci/data-train" \
+                    "../../3_split_train_test/full_split_uci/data-test" \
+                    "../../1_data/uci_qca_sage/qca_sage_data" \
+    --dataset-labels "Training" "Validation" "Sage 2.3.0 Train" \
+    --output-dir "equilibrium_value_analysis" \
+    --n-processes ${N_PROCESSES} 2>&1 | tee check_equil_log.txt
---
+++ 6a_compare_equil/b_no_bond_with_ring_in_atom/submit.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#SBATCH --job-name=EquCheck  ## job name
+#SBATCH -p standard              ## use free partition
+#SBATCH -t 1-00:00:00
+#SBATCH --nodes=1            ## use 1 node, don't ask for multiple
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task=32
+#SBATCH --account DMOBLEY_LAB
+#SBATCH --mem-per-cpu=4G     ## ask for 1Gb memory per CPU
+#SBATCH --constraint=fastscratch
+#SBATCH -o stdout.txt
+#SBATCH -e stderr.txt
+
+export OMP_NUM_THREADS=1
+N_PROCESSES=${SLURM_CPUS_PER_TASK:-1}
+
+date
+hn=`hostname`
+echo "Running job on host $hn"
+echo "JobID: ${SLURM_JOB_ID}"
+echo "Running with ${N_PROCESSES} processes"
+
+source ~/.bashrc
+mamba activate bts
+
+# Run analysis
+python -u ../check_equil_values.py \
+    --offxml "../../6_run_fit/b_no_bond_with_ring_in_atom/final-force-field.offxml" \
+    --dataset-paths "../../3_split_train_test/full_split_uci/data-train" \
+                    "../../3_split_train_test/full_split_uci/data-test" \
+                    "../../1_data/uci_qca_sage/qca_sage_data" \
+    --dataset-labels "Training" "Validation" "Sage 2.3.0 Train" \
+    --output-dir "equilibrium_value_analysis" \
+    --n-processes ${N_PROCESSES} 2>&1 | tee check_equil_log.txt
+
+echo "Analysis complete!"
+date
