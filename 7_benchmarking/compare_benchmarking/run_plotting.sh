ee8fea88df34908acc291c33d89989c0873f6edc
Jennifer A Clark <jennifer.clark@omsf.io>
2026-01-30 11:35:17 -0500

    Fit b3: low learning rate (#50)
    * Fit b3: low learning rate
    
    * Compare b3 with other benchmarks
    
    * Add benchmarking for b3
    
    * Update benchmarking
    
    * Add distribution comparison of b3
    
    * Check ff0 distribution
    
    ---------
    
    Co-authored-by: Jennifer Clark <jennifer.clark@openforcefield.org>

--- 7_benchmarking/compare_benchmarking/run_plotting.sh
+++ 7_benchmarking/compare_benchmarking/run_plotting.sh
@@ -6,20 +6,24 @@ python ../plot_benchmark.py \
         ../0b_openff_2.3.0_smee_smirks/benchmark_results/metrics.json \
         ../b_no_bond_with_ring_in_atom/benchmark_results/metrics.json \
         ../b2_no_bond_with_ring_in_atom/benchmark_results/metrics.json \
+        ../b3_no_bond_with_ring_in_atom/benchmark_results/metrics.json \
     --labels \
         "OpenFF 2.3.0-rc2" \
         "OpenFF 2.3.0-rc2 SMEE-SPICE" \
         "SpecificGenBonds-b1" \
         "SpecificGenBonds-b2" \
+        "SpecificGenBonds-b3" \
     --param-json \
         ../0a_openff_2.3.0-rc2/benchmark_results/metrics_by_parameter_type.json \
         ../0b_openff_2.3.0_smee_smirks/benchmark_results/metrics_by_parameter_type.json \
         ../b_no_bond_with_ring_in_atom/benchmark_results/metrics_by_parameter_type.json \
         ../b2_no_bond_with_ring_in_atom/benchmark_results/metrics_by_parameter_type.json \
+        ../b3_no_bond_with_ring_in_atom/benchmark_results/metrics_by_parameter_type.json \
     --offxml \
         ../../forcefields/openff_unconstrained-2.3.0-rc2.offxml \
         ../../6_run_fit/0_openff_2.3-rc2/final-force-field.offxml \
         ../../6_run_fit/b_no_bond_with_ring_in_atom/final-force-field.offxml \
         ../../6_run_fit/b2_no_bond_with_ring_in_atom/final-force-field.offxml \
+        ../../6_run_fit/b3_no_bond_with_ring_in_atom/final-force-field.offxml \
     --output-dir . \
     -vv 2>&1 | tee plotting_log.txt
