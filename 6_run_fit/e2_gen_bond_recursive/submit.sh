fff840cbdd24adba3c018bfedfb5c98a5ba9c4e9
Jennifer Clark <jennifer.clark@openforcefield.org>
2026-03-09 11:39:41 -0700

    ff-e and ff-e2 fit

---
+++ 6_run_fit/e2_gen_bond_recursive/submit.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#SBATCH --job-name=e_ff  ## job name
+#SBATCH --account DMOBLEY_LAB_GPU
+#SBATCH -p gpu              ## use free partition
+#SBATCH -t 4-00:00:00
+#SBATCH --nodes=1            ## use 1 node, don't ask for multiple
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:1
+#SBATCH --mem=16gb                  # request 16Gb of memory
+#SBATCH --constraint=fastscratch
+#SBATCH --export ALL
+#SBATCH -o stdout_multi.txt
+#SBATCH -e stderr_multi.txt
+
+date
+hn=$(hostname)
+echo "Running job on host $hn"
+ncpus=$SLURM_CPUS_ON_NODE
+echo "$ncpus allocated CPUs"
+
+nvcc --version
+nvidia-smi
+
+export OMP_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+
+source ~/.bashrc
+mamba activate bts
+echo "$(which python)"
+python -c "import torch; print(f'CUDA available: {torch.cuda.is_available()}'); print(f'CUDA version: {torch.version.cuda}'); print(f'Number of GPUs: {torch.cuda.device_count()}')"
+
+python ../fit_data.py --data-dir "../../3_split_train_test/full_split_uci/data-train" \
+                      --filename-forcefield "../../5_setup_train_ff_topologies/uci_off_e/smee_force_field.pkl" \
+                      --filename-topo-dict "../../5_setup_train_ff_topologies/uci_off_e/smee_topology_dict.pkl" \
+                      --offxml "../../4_make_offxmls/e_gen_bond_recursive/openff-2.3.0-gen_bond_atom_rec1.offxml" \
+                      --n-epochs 1000 \
+                      --to-cuda true \
+                      --valence-types "Bonds" \
+                      --learning-rate 0.001 2>&1 | tee log.txt
