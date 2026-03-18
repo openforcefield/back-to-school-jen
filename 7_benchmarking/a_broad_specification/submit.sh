8b7c03d78d4ba3f739fd235a43bd48642bd2945f
Jennifer A Clark <jennifer.clark@omsf.io>
2026-03-18 16:32:39 -0400

    Force Field A: Hyperspecific bonds with explicit bond orders (#67)
    * Make and benchmark general bond ff-a
    ---------
    
    Co-authored-by: Jennifer Clark <jennifer.clark@openforcefield.org>

---
+++ 7_benchmarking/a_broad_specification/submit.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --job-name=Benchmark  ## job name
+#SBATCH -p standard              ## use free partition
+#SBATCH -t 2-00:00:00
+#SBATCH --nodes=1            ## use 1 node, don't ask for multiple
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task=32
+#SBATCH --account DMOBLEY_LAB
+#SBATCH --mem-per-cpu=4G     ## ask for 1Gb memory per CPU
+#SBATCH --constraint="intel&fastscratch"
+#SBATCH -o stdout.txt
+#SBATCH -e stderr.txt
+
+date
+hn=`hostname`
+echo "Running job on host $hn"
+
+source ~/.bashrc
+mamba activate bts
+
+python ../benchmarking.py --offxml "../../6_run_fit/a_broad_specification/final-force-field.offxml" \
+                          --input-dataset "$HOME/bin/yammbs-dataset-submission/datasets/OpenFF-Industry-Benchmark-Season-1-v1.2/cache.json" \
+                          --force-field-name "SpecificGenBondsA" \
+                          --output-dir benchmark_results \
+			  --n-processes $SLURM_CPUS_ON_NODE \
+                          -vvv 2>&1 | tee log.txt
