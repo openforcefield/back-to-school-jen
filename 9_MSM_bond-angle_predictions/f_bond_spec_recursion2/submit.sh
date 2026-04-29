b41c5a4ae36efaac74f70ce2e6473be1c28d857d
Jennifer Clark <jennifer.clark@openforcefield.org>
2026-04-29 07:02:58 -0700

    Attempt ff-f msm

--- 9_MSM_bond-angle_predictions/f_bond_spec_recursion2/submit.sh
+++ 9_MSM_bond-angle_predictions/f_bond_spec_recursion2/submit.sh
@@ -2,10 +2,10 @@
 
 #SBATCH --job-name=MSM-f  ## job name
 #SBATCH -p standard              ## use free partition
-#SBATCH -t 3-00:00:00
+#SBATCH -t 7-00:00:00
 #SBATCH --nodes=1            ## use 1 node, don't ask for multiple
 #SBATCH --ntasks 1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=48
 #SBATCH --account DMOBLEY_LAB
 #SBATCH --mem-per-cpu=4G     ## ask for 1Gb memory per CPU
 #SBATCH --constraint="intel&fastscratch"
@@ -19,8 +19,8 @@ echo "Running job on host $hn"
 source ~/.bashrc
 mamba activate bts
 
-python ../generate-msm-forcefield.py --input-forcefield ../../4_make_offxmls/g_gen_recursion2/openff-2.3.0-bondanglegen-recursion2.offxml \
-                                     --output-forcefield openff-ff-g-msm.offxml \
+python ../generate-msm-forcefield.py --input-forcefield ../../4_make_offxmls/f_broad_spec_recursion2/openff-2.3.0-bondangletypes-recursion2.offxml \
+                                     --output-forcefield openff-ff-f-msm.offxml \
                                      --output-msm msm-out.json \
                                      --msm-data-directory ../msm-data \
                                      --n-workers $SLURM_CPUS_ON_NODE 2>&1 | tee log.txt
